9eba4bdcf6aab0e56ef29ef0ceaba6ea92a875ba
lfahmy <lfahmy38@gmail.com>
2018-06-05 18:16:58 -0700

    num flat features

--- models.py
+++ models.py
@@ -8,23 +8,25 @@ class classifier(nn.Module):
         ## WRITE YOUR CODE HERE, Define the network structure ##
         self.conv1 = nn.Conv2d(1, 32, 5, padding=2)
         self.conv2 = nn.Conv2d(32, 64, 5, padding=2)
-        self.fc1 = nn.Linear(64*7*7, 1024)
+        self.fc1 = nn.Linear(64*5*5, 1024)
         self.fc2 = nn.Linear(1024, 10)
 
     def forward(self, x):
         ## WRITE YOUR CODE HERE ##
         x = F.relu(self.conv1(x), inplace=True)
-        print("relu conv1", x)
         x = F.max_pool2d(x, 2, stride=2)
-        print("first pool", x)
         x = F.relu(self.conv2(x), inplace=True)
-        print("relu conv2", x)
         x = F.max_pool2d(x, 2, stride=2)
-        print("second pool", x)
+        x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x), inplace=True)
-        print("relu fc1", x)
         if(isDropOut):
         	x = F.dropout2d(x, p=.4)
         x = F.softmax(self.fc2(x))
         return x
 
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
